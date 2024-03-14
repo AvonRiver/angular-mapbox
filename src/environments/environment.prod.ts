e9ea7a597e00032cf7399a69448fdf93c7fdae9a
Stephen Webb <steve.j.webb@blueyonder.co.uk>
2024-03-14 08:52:36 +0000

    Draft version

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    mapbox: {
+        accessToken: 'pk.eyJ1Ijoid2Vib2Zyb2FkcyIsImEiOiJjbHRxeTA4ODUwZDd4MmxwM3F0eDZnbDV0In0.B9mMs2J_rHf-bfhNTr52rg' // Your productionaccess token goes here
+      }
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    mapbox: {
+      accessToken: 'pk.eyJ1Ijoid2Vib2Zyb2FkcyIsImEiOiJjbHRxeTA4ODUwZDd4MmxwM3F0eDZnbDV0In0.B9mMs2J_rHf-bfhNTr52rg' // Your access token goes here
+    }
+  };
